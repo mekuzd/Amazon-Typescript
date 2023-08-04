8f38d2094ab3efd7ac93a781e4c38d7f50fa6cb2
emeka oluwaseun <emekaseun.es@gmail.com>
2023-08-04 15:44:13 +0100

    added bootstrap as ui component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,42 @@
-import "./App.css";
+import { Navbar, Container, Row, Col, Nav } from "react-bootstrap";
 import { sampleProducts } from "./data";
 
 function App() {
   return (
-    <>
+    <div className="d-flex flex-column vh-100 ">
+      {" "}
       <header>
-        <h1>Amazon</h1>
+        <Navbar bg="dark" variant="dark" expand="lg">
+          <Container>
+            <Navbar.Brand>AmazonTs</Navbar.Brand>
+          </Container>
+          <Nav>
+            <a className="nav-link" href="">
+              Cart
+            </a>
+            <a className="nav-link" href="">
+              Login
+            </a>
+          </Nav>
+        </Navbar>
       </header>
       <main>
-        <ul>
-          {sampleProducts.map((products) => (
-            <li key={products.slug}>
-              <img src={products.image} alt="" className="product-image" />
-              <h2>{products.name}</h2>
-              <p>${products.price}</p>
-            </li>
-          ))}
-        </ul>
+        <Container className="mt-3">
+          <Row>
+            {sampleProducts.map((products) => (
+              <Col key={products.slug} sm={6} md={4} lg={3}>
+                <img src={products.image} alt="" className="product-image" />
+                <h2>{products.name}</h2>
+                <p>${products.price}</p>
+              </Col>
+            ))}
+          </Row>
+        </Container>
       </main>
-    </>
+      <footer>
+        <div className="text-center">All right reserved</div>
+      </footer>
+    </div>
   );
 }
 
