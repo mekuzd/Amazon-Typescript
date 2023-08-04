f8f60ed2cb09623798297d030cbff2804fd9ff71
emeka oluwaseun <emekaseun.es@gmail.com>
2023-08-04 15:08:28 +0100

    added types folder and created data file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,23 @@
 import "./App.css";
+import { sampleProducts } from "./data";
 
 function App() {
   return (
     <>
-      <h1>Amazon</h1>
+      <header>
+        <h1>Amazon</h1>
+      </header>
+      <main>
+        <ul>
+          {sampleProducts.map((products) => (
+            <li key={products.slug}>
+              <img src={products.image} alt="" className="product-image" />
+              <h2>{products.name}</h2>
+              <p>${products.price}</p>
+            </li>
+          ))}
+        </ul>
+      </main>
     </>
   );
 }
