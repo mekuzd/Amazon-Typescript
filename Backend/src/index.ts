d5dd8aed47d85827264fa8a8cfdc26e1ba4914d9
emeka oluwaseun <emekaseun.es@gmail.com>
2023-08-10 01:01:30 +0100

    added cors to backend

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from "express";
 import { sampleProducts } from "./data";
 const app = express();
+import cors from "cors";
 const port = 5000;
+app.use(cors());
 app.get("/api/products", (req: Request, res: Response) => {
   res.json(sampleProducts);
 });
