49f3374e69174a1a8de927ae17f3655ede73a6a5
emeka oluwaseun <emekaseun.es@gmail.com>
2023-08-11 14:13:44 +0100

    created new end point

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -7,6 +7,11 @@ app.use(cors());
 app.get("/api/products", (req: Request, res: Response) => {
   res.json(sampleProducts);
 });
+app.get("/api/products/:slug", (req: Request, res: Response) => {
+  const { slug } = req.params;
+  res.json(sampleProducts.find((product) => product.slug === slug));
+});
+
 app.listen(port, () => {
   console.log(`app listening on ${port}`);
 });
