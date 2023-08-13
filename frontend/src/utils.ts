ee47cd255ea92e23ea89c13905b266bde41f9835
emeka oluwaseun <emekaseun.es@gmail.com>
2023-08-13 12:25:28 +0100

    adjusted util.ts

--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -1,23 +1,7 @@
-import { Action } from "./Types/Action";
 import { ApiError } from "./Types/apiError";
-import { State } from "./Types/state";
 
 export const getError = (error: ApiError) => {
   return error.message && error.response.data.message
     ? error.response.data.message
     : error.message;
-};
-
-export const reducer = (state: State, action: Action) => {
-  if (action.type == "FETCH_REQUEST") {
-    return { ...state, loading: true };
-  } else if (action.type == "FETCH_SUCCESS") {
-    return { ...state, products: action.payload, loading: false };
-  } else if (action.type == "FETCH_FAIL") {
-    return { ...state, error: action.payload, loading: false };
-  } else if (action.type == "FETCH_PRODUCT") {
-    return { ...state, product: action.payload, loading: false };
-  } else {
-    return state;
-  }
 };
