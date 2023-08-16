9fe6b72b1d7367c3385e3a6d570c488d37e6a7a6
emeka oluwaseun <emekaseun.es@gmail.com>
2023-08-16 14:04:22 +0100

    adjusted app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,9 @@
 import { Container } from "react-bootstrap";
 import Homepage from "./Pages/Hompage";
 import DefaultLayout from "./Components/DefaultLayout";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 function App() {
   return (
     <div className="d-flex flex-column vh-100 ">
-      <ToastContainer position="bottom-center" limit={1} />
       <DefaultLayout>
         <main>
           <Container className="mt-3 ">
