d7d5e35dae597866803d90672a60e7622a87f110
emeka oluwaseun <emekaseun.es@gmail.com>
2023-08-22 01:28:56 +0100

    created signup router

--- Backend/src/Routes/userRouter.ts
+++ Backend/src/Routes/userRouter.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from "express";
 import asyncHandler from "express-async-handler";
-import { userModel } from "../Models/userModel";
+import { User, userModel } from "../Models/userModel";
 import bcrypt from "bcryptjs";
 import { generateToken } from "../utils";
 export const userRouter = express.Router();
@@ -23,5 +23,24 @@ userRouter.post(
       }
     }
     res.status(401).json({ message: "Invalid email or password" });
+  }),
+);
+
+userRouter.post(
+  "/signup",
+  asyncHandler(async (req: Request, res: Response) => {
+    const user = await userModel.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password),
+    } as User);
+
+    res.send({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user),
+    });
   }),
 );
